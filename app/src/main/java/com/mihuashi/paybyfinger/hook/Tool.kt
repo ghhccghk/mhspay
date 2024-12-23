5d6d0e2a63ef0449de1175dc09950f3d60037f5f
ghhccghk <2137610394@qq.com>
2024-12-23 21:28:15 +0800

    优化设备名称显示

--- app/src/main/java/com/mihuashi/paybyfinger/hook/Tool.kt
+++ app/src/main/java/com/mihuashi/paybyfinger/hook/Tool.kt
@@ -2,12 +2,20 @@ package com.mihuashi.paybyfinger.hook
 
 import android.annotation.SuppressLint
 import android.os.Build
+import android.util.Log
 
 object  Tool {
     val getPhoneName by lazy {
         val marketName = getSystemProperties("ro.product.marketname")
-        if (marketName.isNotEmpty()) marketName else Build.BRAND + Build.MODEL}
+        if (marketName.isNotEmpty()) bigtextone(marketName) else bigtextone(Build.BRAND) + " " + Build.MODEL
+    }
 
+    fun bigtextone(st:String): String {
+        val formattedBrand = st.replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase() else it.toString()
+        }
+        return formattedBrand
+    }
 
     @SuppressLint("PrivateApi")
 
