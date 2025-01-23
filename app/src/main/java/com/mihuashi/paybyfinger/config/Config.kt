80a067f34b810ac8a00b36a298af1431cc31d294
ghhccghk <2137610394@qq.com>
2025-01-23 17:17:22 +0800

    将系统信息获取分离成单独Class

--- app/src/main/java/com/mihuashi/paybyfinger/config/Config.kt
+++ app/src/main/java/com/mihuashi/paybyfinger/config/Config.kt
@@ -1,8 +1,6 @@
 package com.mihuashi.paybyfinger.config
 
-import android.os.Build
 import cn.xiaowine.dsp.delegate.Delegate.serialLazy
-import com.mihuashi.paybyfinger.hook.Tool.getSystemProperties
 
 class Config {
     var hidesetting: Boolean by serialLazy(false)
