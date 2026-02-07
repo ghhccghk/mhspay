adb0d248bb5e9e1da863768c46bb687fdfb1c9ba
ghhccghk <2137610394@qq.com>
2026-02-07 21:38:25 +0800

    更新应用版本至1.0.5，并修改README说明
    Signed-off-by: ghhccghk <2137610394@qq.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,9 @@ android {
     defaultConfig {
         applicationId = "com.mihuashi.paybyfinger"
         minSdk = 28
-        versionCode = 5
-        versionName = "1.0.4"
+        targetSdk = 36
+        versionCode = 6
+        versionName = "1.0.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         dependenciesInfo.includeInApk = false
         ndk.abiFilters += arrayOf("arm64-v8a", "armeabi-v7a", "x86", "x86_64","armeabi","mips", "mips64")
