fda0601c284f664cbc51cbced33d8c749da81046
ghhccghk <2137610394@qq.com>
2026-05-12 15:18:44 +0800

    更新应用版本至 1.1.1 (versionCode 8)
    Signed-off-by: ghhccghk <2137610394@qq.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     defaultConfig {
         applicationId = "com.mihuashi.paybyfinger"
         minSdk = 28
-        versionCode = 7
-        versionName = "1.1.0"
+        versionCode = 8
+        versionName = "1.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         dependenciesInfo.includeInApk = false
         ndk.abiFilters += arrayOf("arm64-v8a", "armeabi-v7a", "x86", "x86_64","armeabi","mips", "mips64")
